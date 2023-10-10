d7e7d1e6673b8fab1f9d167379aa8f6b5fbfaa2d
Robert Siński <75808735+Biplo12@users.noreply.github.com>
2023-10-10 18:10:56 +0200

    feat: X icon

--- src/components/Buttons/TaskCreate.vue
+++ src/components/Buttons/TaskCreate.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="border border-green-500 px-4 py-2 duration-150 ease-linear hover:bg-green-500"
+    class="border border-green-500 px-4 py-2 duration-150 ease-linear hover:bg-green-500 hover:text-black"
     @click="taskCreateHandler"
   >
     Create Task
--- src/components/Items/TodoItem.vue
+++ src/components/Items/TodoItem.vue
@@ -1,16 +1,20 @@
 <template>
   <div
-    :class="`p-4 border-l-2 flex flex-col gap-2 bg-white bg-opacity-5 w-80   ${
+    :class="`p-4 border-l-2 flex flex-col gap-2 bg-white bg-opacity-5 w-80 relative ${
       item.isDone ? 'border-green-500' : 'border-white'
     } `"
   >
     <h3 class="opacity-25 text-sm">{{ formattedCreatedAt }}</h3>
     <h1 class="text-xl font-bold">{{ item.title }}</h1>
     <p class="opacity-75">{{ item.description }}</p>
+    <button class="absolute right-3 top-3 text-red-500">
+      <X />
+    </button>
   </div>
 </template>
 
 <script lang="ts">
+import { X } from 'lucide-vue-next'
 interface ITodoItem {
   title: string
   description: string
@@ -20,6 +24,9 @@ interface ITodoItem {
 
 export default {
   name: 'TodoItem',
+  components: {
+    X
+  },
   props: {
     item: {
       type: Object as () => ITodoItem,
